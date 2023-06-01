29adca9d239a7abe8565f8266176e1f26a90fe36
RUZZIN <125768983+RUZZIN@users.noreply.github.com>
2023-05-31 22:37:41 -0300

    Atualização de dados, dados enviados dia 31/05/2023

---
+++ AULA-19-EVENTOS/mouse.js
@@ -0,0 +1,19 @@
+function acao() {
+    alert('Primeiro evento disparado')
+}
+
+function acao2() {
+    alert('Segundo evento disparado')
+}
+
+function acao3() {
+    alert('Terceiro evento disparado')
+}
+
+function acao4() {
+    alert('Quarto evento disparado')
+}
+
+function acao5() {
+    alert('Quinto evento disparado')
+}
---
+++ AULA-19-EVENTOS/teclado.js
@@ -0,0 +1,11 @@
+function acao1() {
+    alert('Primeiro evento disparado')
+}
+
+function acao2() {
+    alert('Segundo evento disparado')
+}
+
+function acao3() {
+    alert('Terceiro evento disparado')
+}
