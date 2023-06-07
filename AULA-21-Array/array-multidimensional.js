175607650f402bc302e94a4e7cd3f42be909cdc6
Ruan Pablo Mesquita <125768983+RUZZIN@users.noreply.github.com>
2023-06-07 17:28:17 -0300

    Envio de dados, Array multidimensional

---
+++ AULA-21-Array/array-multidimensional.js
@@ -0,0 +1,16 @@
+var lista_coisas = Array()
+
+lista_coisas['frutas'] = Array('Bananas','Maça','Morango','Uva')
+
+/*lista_coisas['frutas'][1] = 'Bananas'
+lista_coisas['frutas'][2] = 'Maça'
+lista_coisas['frutas'][3] = 'Morango'
+lista_coisas['frutas'][4] = 'Uva'*/
+
+lista_coisas['pessoas'] = []
+
+lista_coisas['pessoas']['a'] = 'João'
+lista_coisas['pessoas']['b'] = 'José'
+lista_coisas['pessoas']['c'] = 'Maria'
+
+document.write(lista_coisas['frutas'][3])
